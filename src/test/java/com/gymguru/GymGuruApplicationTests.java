064fb14c6992b49718bfb866beede94b93e63fdb
Karol002 <karol.wojcik.002@gmail.com>
2023-03-25 11:52:38 +0100

    First commit

---
+++ src/test/java/com/gymguru/GymGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
